c03244fc98827414a48d4ac569a58437cfa604f2
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-03-15 10:04:27 +0000

    the shell correctly executes local exe files and exit is fixed

---
+++ src/head.c
@@ -0,0 +1,111 @@
+/*
+    name: Ismail Elbahja
+    email: ismailelbahja03@gmail.com
+    student number: 21344991
+
+    I have implemented the head utility, it accepts even/odd option and a specified amount of lines to print.
+    All the options work correctly but I have not implemented a feature of head where if a negative number is passed for n
+        the head funtion prints until EOF-n lines, instead my implementation just prints the whole file when given a negative n
+    The programme acts like the default head function when a file is not specifed and reads from standard in instead,
+        I tested it by piping an the output of grep into it and it worked as expected.
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void parityerror();
+void printversion();
+void printhelp();
+
+int main(int argc, char *argv[]) {
+
+    int use_odd = 0, use_even = 0, num_lines = 10, opt;
+
+    while ( ( opt = getopt(argc, argv, "n:Vheo" ) ) != -1) {
+        switch (opt) {
+            case 'n':
+                num_lines = atoi(optarg);
+                break;
+            case 'V':
+                printversion();
+                exit(EXIT_SUCCESS);
+                break;
+            case 'h':
+                printhelp();
+                exit(EXIT_SUCCESS);
+                break;
+            case 'e':
+                if (use_odd) {parityerror();}
+                use_even = 1;
+                break;
+            case 'o':
+                if (use_even) {parityerror();}
+                use_odd = 1;
+                break;
+            default:
+                fprintf(stderr, "unknown commands, try : %s -h\n", argv[ 0 ]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+
+    FILE *fp;
+    char *filename = NULL;
+
+    if (argc > optind) {
+        filename = argv[optind];
+    }
+
+    if (filename == NULL) {
+        fp = stdin;
+    } else {
+        fp = fopen (filename, "r");
+        if (fp == NULL) {
+            fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
+            exit(EXIT_FAILURE);
+        }
+
+    }
+
+
+    int i = 1, printed_lines =0;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        if (num_lines > 0 && printed_lines == num_lines) break;
+
+
+        if (use_odd == 1 && i %2 == 1 ||
+            use_even == 1 && i %2 == 0 ||
+            use_even == 0 && use_odd == 0) {
+
+            printf("%s", line); printed_lines++;
+        }
+        i++;
+    }
+
+    fclose(fp);
+    free(line);
+    exit(EXIT_SUCCESS);
+
+}
+
+void printversion() {
+    printf("version No.: v2.3 \nname: Ismail \nemail: ismailelbahja03@gmail.com \nstudent number: 21344991 \n");
+}
+
+void parityerror() {
+    fprintf(stderr, "Can't choose both even and odd, must choose one or neither \n");
+    exit(EXIT_FAILURE);
+}
+
+void printhelp() {
+    printf("%s \n%s \n%s \n%s \n", "-n K output the first K lines",
+           "-V Output version info: Your name, email, and student number.",
+           "-h display all options ( something like this text describing the options ) and exit the application",
+           "-e|-o print even|odd lines.");
+}
--- src/main.c
+++ src/main.c
@@ -8,14 +8,18 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#define MAX_LINE 80 /* The maximum length command */
+#define MAX_LINE 80 // maximum length of a command
 extern char **environ;
 
 int shell();
 
+void readline(char *line);
+
+void parse_args(char **args, char *token);
+
 int main(void)
 {
-    int status = 1; /* status of child process */
+    int status = 1; // status of child process
 
     while (status == 1) {
         status = shell();
@@ -32,42 +36,57 @@ int shell() {
 
     // read commands from stdin
     char line[MAX_LINE];
-    fgets(line, sizeof(line), stdin);
+    readline(line);
 
     // parse input
     char **args = malloc(sizeof(char *));
     char *token = strtok(line, " ");
+    parse_args(args, token);
+
+
+    // exit shell if user enters "exit"
+    if (strcmp(args[0], "exit") == 0) {
+        free(args);
+        return -1;
+    }
+
+    pid_t pid = fork();
+    if (pid == 0) { // child process
+        if (execve(args[0], args, NULL) < 0) { // execute command
+            fprintf(stderr, "Exec failed\n");
+        }
+    } else if (pid > 0) { // parent process
+        int status;
+        waitpid(pid, &status, 0); // wait for child process to finish
+    } else {
+        return -1; //if fork fails then stop programme
+    }
+
+    return 1;
+}
+
+void readline(char *line) {
+    fflush(stdin);
+    fgets(line, MAX_LINE, stdin);
+
+    // trim newline character
+    size_t len = strlen(line);
+    if (len > 0 && line[len-1] == '\n') {
+        line[len-1] = '\0';
+    }
+}
+
+void parse_args(char **args, char *token) {
     int count = 0;
     while (token != NULL) {
-        args[count++] = token;
-        args = realloc(args, (count + 1) * sizeof(char *));
-        token = strtok(NULL, " ");
+        args[count] = malloc(sizeof(char) * (strlen(token) + 1));
+        strcpy(args[count], token);
+        count++;
+        token = strtok(NULL, " \n");
     }
     args[count] = NULL;
 
     for (int j = 0; j < count; ++j) {
         printf("%s ", args[j]);
     }
-
-
-    // exit shell if user enters "exit"
-    if (strcmp(args[0], "exit") == 0) {
-        return -1;
-    }
-
-    pid_t pid = fork();
-    if (pid == 0) { // child process
-        if (execve(args[0], args, environ) < 0) { // execute command
-            fprintf(stderr, "Exec failed\n");
-            return 1;
-        }
-    } else if (pid > 0) { // parent process
-        int status;
-        waitpid(pid, &status, 0); /* wait for child process to finish */
-    } else {
-        return 0;
-    }
-
-    return 1;
 }
-
